94bc10915f5d9d20e5ff19ccd7e57aa56814aff7
Sharoon ck <ck.sharoon@gmail.com>
2024-10-12 14:24:03 +0530

    code optimization

--- StringCalculator.js
+++ StringCalculator.js
@@ -7,33 +7,30 @@ class StringCalculator {
         try {
             this.callCount++;
 
-            if (numbers === "") return 0;
+            if (!numbers) return 0;
 
             const { delimiters, numString } = this.findDelimiters(numbers);
             const delimiterRegex = new RegExp(`[${delimiters.join('')}]|\\n`);
             const numArray = numString.split(delimiterRegex);
             const negatives = [];
-
             const sum = numArray.reduce((total, current) => {
                 const num = parseInt(current, 10);
-
                 if (num < 0) {
                     negatives.push(num);
                 } else if (num <= 1000) {
-                    return total + num; // Accumulate only numbers <= 1000
+                    total += num; // Accumulate only numbers <= 1000
                 }
-
                 return total;
             }, 0);
 
-            if (negatives.length > 0) {
+            if (negatives.length) {
                 throw new Error(`negatives not allowed: ${negatives.join(", ")}`);
             }
 
             return sum;
         }
         catch(error){
-             //console.log('StringCalculator:add:error:', error);
+            //console.log('StringCalculator:add:error:', error);
             throw error;
         }
     }
@@ -49,15 +46,20 @@ class StringCalculator {
         // Check for custom delimiter with any length using brackets
         const customDelimiterMatch = input.match(/^\/\/\[(.+?)\]\n/);
 
+        // Extract delimiters within square brackets
         if (customDelimiterMatch) {
-            // Extract delimiters within square brackets
             const delimiterString = customDelimiterMatch[1];
             delimiters = delimiterString.split('][');
             numString = input.slice(customDelimiterMatch[0].length);
-        } else if (input.startsWith("//")) {
+            return { delimiters, numString };
+        }
+
+        // Check for single custom delimiter
+        if (input.startsWith("//")) {
             const parts = input.split("\n");
             delimiters = [parts[0].substring(2)];
             numString = parts.slice(1).join("\n");
+            return { delimiters, numString };
         }
 
         return { delimiters, numString };
