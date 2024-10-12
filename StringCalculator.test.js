7e774eeb29eab6fbbee86ac80db1bb87c23a4bdc
Sharoon ck <ck.sharoon@gmail.com>
2024-10-12 11:50:39 +0530

    added 2nd test case

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -9,18 +9,23 @@ describe('StringCalculator', () => {
     });
 
     
-//TEST CASE 1
+//TEST CASE 1: basic add operations
 
-    it('should return 0 for an empty string', () => {
+    it('should work for basic add operations', () => {
         assert.strictEqual(calculator.Add(""), 0);
-    });
-
-    it('should return the number itself if only one number is provided', () => {
         assert.strictEqual(calculator.Add("1"), 1);
+        assert.strictEqual(calculator.Add("1,2"), 3);
+        assert.strictEqual(calculator.Add("5,6"), 11);
     });
 
-    it('should return the sum of two numbers separated by a comma', () => {
-        assert.strictEqual(calculator.Add("1,2"), 3);
+// TEST CASE 2: Allow the Add method to handle an unknown amount of numbers
+
+    it('should return the sum of any number of numbers separated by commas', () => {
+        assert.strictEqual(calculator.Add("1,2,3,4,5"), 15);
+        assert.strictEqual(calculator.Add("10,20"), 30); 
+        assert.strictEqual(calculator.Add("1,2,3"), 6); 
+        assert.strictEqual(calculator.Add("100,200,300"), 600);
+        assert.strictEqual(calculator.Add("5,10,15,20,25,30,35,40,45,50,55"), 330);
     });
 
 });
